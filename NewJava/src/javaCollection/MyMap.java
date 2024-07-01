791589e6a6b9dda2a6ffca38e20094211c2f23fa
Bshree-Coder <bhagyashrikorde2510@gmail.com>
2024-07-01 13:02:49 +0530

    I created HashMap Collection
    I created Hasmap object and I have added 5 string key and 5 integer values

---
+++ NewJava/src/javaCollection/MyMap.java
@@ -0,0 +1,23 @@
+package javaCollection;
+
+import java.util.HashMap;
+
+public class MyMap {
+
+	public static void main(String[] args) {
+		
+		
+		HashMap< String, Integer> H = new HashMap<String,Integer>();
+		// TODO Auto-generated method stub
+		H.put("Komal",10);
+		H.put("Bhagyashri",20);
+		H.put("Vijeta", 30);
+		H.put("Simran", 40);
+		H.put("Kiran", 50);
+		System.out.println(H);
+		
+		
+
+	}
+
+}
