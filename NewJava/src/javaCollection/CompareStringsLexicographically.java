335a320494b3b71d1a9359deb2b009cb1db8a146
Bshree-Coder <bhagyashrikorde2510@gmail.com>
2024-07-02 14:09:51 +0530

    Compare string lexicographically
    Here I have created a program to compare two strings lexicographically.

---
+++ NewJava/src/javaCollection/CompareStringsLexicographically.java
@@ -0,0 +1,26 @@
+package javaCollection;
+
+public class CompareStringsLexicographically {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Using compareTo() method
+		
+		String s1="Apple";
+		String s2="Banana";
+		
+		int result=s1.compareTo(s2);
+		if(result<0) {
+			System.out.println( " ' " +s1+ " comes before " +s2+ " '.");
+			
+		}
+		else if(result>0) {
+			System.out.println(s1 + " comes after "+ s2);
+		}
+		else {
+			System.out.println(s1 +"and" +s2+ "are equal");
+		}
+	}
+
+}
