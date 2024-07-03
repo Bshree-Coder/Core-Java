66754f79c3195a612b4fc289992fdc7ac34968c2
Bshree-Coder <bhagyashrikorde2510@gmail.com>
2024-07-03 14:50:17 +0530

    Stack operation on characters
    Here I have created a program to perform stack operation to add characters, search specific character, clear stack.

---
+++ NewJava/src/javaCollection/StackCharacter.java
@@ -0,0 +1,28 @@
+package javaCollection;
+
+import java.util.Stack;
+
+public class StackCharacter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Stack sc=new Stack();
+//		
+//		sc.push('X');
+//		sc.push('Y');
+//		sc.push('Z');
+//		
+//		System.out.println(sc.contains('Y'));
+		
+		sc.push('A');
+		sc.push('B');
+		sc.push('C');
+		
+		System.out.println(sc);
+		sc.clear();
+		//System.out.println(sc);
+
+	}
+
+}
--- NewJava/src/javaCollection/StackEvenNumbers.java
+++ NewJava/src/javaCollection/StackEvenNumbers.java
@@ -8,7 +8,7 @@ public class StackEvenNumbers {
 		 Stack<Integer> sc = new Stack<>();
 	        
 	       
-	        int[] evenNumbers = {2, 4, 6, 8, 10};
+	        int[] evenNumbers = {1,2,3, 4,5, 6, 7,8,9, 10};
 	        
 	        
 	        
